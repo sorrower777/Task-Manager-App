b53aa4b70848ae6531ef23bcef5b05695a752495
Ritom <chakrabortyarindam053@gmail.com>
2026-03-14 10:39:49 +0530

    fix: update default port from 3000 to 5000 in index.js

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -5,7 +5,7 @@ import { ConnectDb } from "./src/db.config.js";
 
 dotenv.config({ path: ".env" });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 ConnectDb();
 
